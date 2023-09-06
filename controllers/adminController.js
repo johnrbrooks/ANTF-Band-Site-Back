c27351a2878baed08592991f2322fab19a24bee9
John Brooks <johnrbrooks93@gmail.com>
2023-09-05 20:47:33 -0400

    Tweaked controller function

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,12 +1,17 @@
 const { Admin } = require('../models')
 
-
 const getAdmin = async (req, res) => {
     try {
-        const admin = await Admin.findOne({ email: req.params });
+        const { email } = req.params
+        const admin = await Admin.findOne({ email: email })
+
+        if(!admin) {
+            return res.status(404).json({ message: "Admin not found." })
+        }
+
         res.json(admin)
     } catch (error) {
-        res.send(error)
+        res.status(500).json({ error: error.message })
     }
 }
 
