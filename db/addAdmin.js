1b7cb0e622191d033175b3edb1dc86c3529152bb
John Brooks <johnrbrooks93@gmail.com>
2026-04-08 23:31:40 -0400

    Small updates to controller functions and routes

--- db/addAdmin.js
+++ db/addAdmin.js
@@ -1,16 +1,17 @@
 const adminUser = {
-    email: 'anighttoforgetdc@gmail.com',
-    password: 'Peopleweknew',
+    email: "anighttoforgetdc@gmail.com",
+    password: "Peopleweknew",
 };
 
 const createAdminUser = async () => {
-    const url = `https://antf-band-site-back-production.up.railway.app/api/admin/createAdmin`;
+    // const url = `https://antf-band-site-back-production.up.railway.app/api/admin/createAdmin`;
+    // const url = "http://localhost:3001/api/admin/createAdmin";
 
     try {
         const response = await fetch(url, {
-            method: 'POST',
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
             },
             body: JSON.stringify(adminUser),
         });
@@ -18,7 +19,7 @@ const createAdminUser = async () => {
         const data = await response.json();
 
         if (!response.ok) {
-            console.error('Request failed:', data);
+            console.error("Request failed:", data);
             return;
         }
 
@@ -26,7 +27,7 @@ const createAdminUser = async () => {
             console.log(data.message);
         }
     } catch (error) {
-        console.log('There was an error: ', error);
+        console.log("There was an error: ", error);
     }
 };
 
--- routes/AdminRouter.js
+++ routes/AdminRouter.js
@@ -1,10 +1,14 @@
-const express = require('express');
+const express = require("express");
 const Router = express.Router();
-const controller = require('../controllers/adminController');
+const controller = require("../controllers/adminController");
 
-Router.get('/get/:email', controller.getAdmin);
+Router.get("/get/:email", controller.getAdmin);
+Router.get("/checkAuth", controller.getCurrentAdmin);
+
+Router.post("/login", controller.loginAdmin);
+Router.post("/logout", controller.logoutAdmin);
 
 // temporary route to create the admin
-Router.post('/createAdmin', controller.createAdmin);
+Router.post("/createAdmin", controller.createAdmin);
 
 module.exports = Router;
