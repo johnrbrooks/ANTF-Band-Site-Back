08862c73dd8dffb86140597eea44ff1e8073143e
John Brooks <johnbrooks@Johns-MacBook-Pro.local>
2026-04-09 14:52:18 -0400

    Cleaning up debug routes

--- routes/AdminRouter.js
+++ routes/AdminRouter.js
@@ -2,14 +2,16 @@ const express = require('express');
 const Router = express.Router();
 const controller = require('../controllers/adminController');
 
-Router.get('/get/:email', controller.getAdmin);
-Router.get('/get', controller.getAdmins);
+// DEBUG ROUTES
+// Router.get('/get/:email', controller.getAdmin);
+// Router.get('/get', controller.getAdmins);
+
 Router.get('/checkAuth', controller.checkAuth);
 
 Router.post('/login', controller.loginAdmin);
 Router.post('/logout', controller.logoutAdmin);
 
 // temporary route to create the admin
-Router.post('/createAdmin', controller.createAdmin);
+// Router.post('/createAdmin', controller.createAdmin);
 
 module.exports = Router;
